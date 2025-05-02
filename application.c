4093f6f1118a5a64a9c42545716dedb5cd575bf4
Nourishere <nournawar5@gmail.com>
2025-05-02 11:34:58 +0300

    Cleaned application.c

--- application.c
+++ application.c
@@ -12,41 +12,12 @@ uint8 Iflag;
  //#define _XTAL_FREQ 8000000
 //This macro is used in the __delay_ms() function
 
-INT_RBx_t first_int = {
-	NULL,
-        NULL,
-	{PORTB_I, PIN3, GPIO_IN, GPIO_LOW},
-	INT_PLOW
-};
-INT_INTx_t second_int = {
-	NULL,
-	{PORTB_I, PIN1, GPIO_IN, GPIO_LOW},
-        rising,
-	INT_PLOW
-};
-
-pin_config_t seg_units_en = { 
-	PORTD_I,
-	PIN1,
-	GPIO_OUT,
-	GPIO_HIGH
-};
-
-uint8 i=90, j=0, knum=0, prev;
 
 int main(void){
     STD_ReturnType ret = E_NOT_OK;
-    if(INT_INTx_initialize(&second_int))
-        LED_on(&LED_OK);
-    else
-        LED_on(&LED_NOK);
-   
- //   ret = lcd_send_char_data_position(&LCD1,1,4,'A');
-//    ret = lcd_send_command(&LCD1,LCD_8BIT_1LINE_5x10);
-//	ret = lcd_send_string(&LCD1,"Nour");
-        while(1){
-          
-		}
+    while(1){
+      
+	}
     
 }
 
