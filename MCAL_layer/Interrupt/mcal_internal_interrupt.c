3f31866335f59f6730d7c21b7d46991a32c6c9a6
Nourishere <nournawar5@gmail.com>
2025-05-29 12:52:23 +0300

    Added support for the ADC module

--- MCAL_layer/Interrupt/mcal_internal_interrupt.c
+++ MCAL_layer/Interrupt/mcal_internal_interrupt.c
@@ -5,5 +5,77 @@
  * Created on April 15, 2025, 2:24 PM
  */
 #include "mcal_internal_interrupt.h"
-#include "mcal_interrupt_manager.h"
 
+#if (INT_ADC == INT_EN)
+void (*ADC_callback) (uint16 * result) = NULL;
+uint16 ADC_output;
+#endif
+void ADC_ISR(void){
+	INT_ADC_CLRF();
+	if(ADC_callback){
+		ADC_output = (uint16) ( (((uint16)(ADRESH)) << 8) | ((uint16) (ADRESL)) );
+		ADC_callback(&ADC_output);
+	}
+}
+#if (INT_ADC == INT_EN)
+/* @brief: Initialize the ADC interrupt.
+ * @param: A pointer to an ADC_t type.
+ * @return: E_OK upon success and E_NOT_OK otherwise.
+ */
+STD_ReturnType INT_ADC_init(uint8 priority){
+	STD_ReturnType ret = E_OK;
+		INT_ADC_CLRF();	
+		INT_ADC_DIS();
+#if (INT_PR == INT_EN)
+		if(priority != INT_PHIGH && priority != INT_PLOW)
+			ret = E_NOT_OK;
+		else{
+            INT_PREN(); /* enable the priority feature */
+	 		/* @ref: hal_interrupt_config.h -> INT_PHIGH & INT_PLOW */
+			if(priority == INT_PHIGH){
+				INT_ADC_HP(); /* Enable ADC as a high priority */
+				INT_GHPEN(); /* Enable global high interrupts */
+			}
+			else if(priority == INT_PLOW){
+				INT_ADC_LP();
+				INT_GLPEN();
+			}
+			
+			else
+				ret = E_NOT_OK;
+                        
+           }
+		
+#elif (INT_PR == INT_DIS)
+        INT_PRDIS(); /* disable the priority feature */
+		INT_GEN(); /* General interrupt enable */
+		INT_PEEN(); /* Peripheral interrupt enable */
+#else 
+		ret = E_NOT_OK;
+#endif
+		INT_ADC_EN(); /* Enable the ADC peripheral */
+
+	return ret;
+}
+
+/* @brief: De-initialize the ADC interrupt. 
+ * @param: This function does not take any input.
+ * @return: E_OK upon success and E_NOT_OK otherwise.
+ */
+STD_ReturnType INT_ADC_deinit(void){
+	STD_ReturnType ret = E_OK;
+	INT_ADC_CLRF();
+	INT_ADC_DIS();
+	return ret;
+}
+
+/* @brief: Set up the callback function for the ADC interrupt.
+ * @param: A pointer to a ADC_t type.
+ * @return: E_OK upon success and E_NOT_OK otherwise.
+ */
+STD_ReturnType INT_ADC_set_callback_routine(void (*callback) (uint16 * result)){
+	STD_ReturnType ret = E_OK;
+		ADC_callback = callback;	
+	return ret;
+}
+#endif
--- MCAL_layer/Interrupt/mcal_internal_interrupt.h
+++ MCAL_layer/Interrupt/mcal_internal_interrupt.h
@@ -1,13 +1,42 @@
 /* 
  * File:   mcal_internal_interrupt.h
  * Author: nour
- *
+ * Brief: This header file contains the configuration of
+ * 		  all internal and peripheral interrupt features.
  * Created on April 15, 2025, 2:25 PM
  */
 
 #ifndef MCAL_INTERNAL_INTERRUPT_H
 #define	MCAL_INTERNAL_INTERRUPT_H
+/***** includes ********/
+#include "mcal_interrupt_config.h"
+/***** macros *********/
+/************ ADC interrupt *****************/
+/* Status of the ADC (EN/DIS) */
+#define INT_ADC_STATUS (PIE1bits.ADIE)
+/* The value of the ADC flag */
+#define INT_ADC_F (PIR1bits.ADIF)
+/****** function-like macros ******/
+/************ ADC interrupt *****************/
+#if (INT_ADC == INT_EN)
+/* Enable/Disable the ADC interrupt */
+#define INT_ADC_EN() (PIE1bits.ADIE=1)
+#define INT_ADC_DIS() (PIE1bits.ADIE=0)
+/* Clear the ADC interrupt flag */
+#define INT_ADC_CLRF() (PIR1bits.ADIF=0)
+/* Set ADC low/high priority */
+#if (INT_PR == INT_EN)
+#define INT_ADC_HP() (IPR1bits.ADIP=1)
+#define INT_ADC_LP() (IPR1bits.ADIP=0)
+#endif
+#endif
 
-
+/***** Function prototypes *****/
+void ADC_ISR(void);
+#if (INT_ADC == INT_EN)
+STD_ReturnType INT_ADC_init(uint8 priority);
+STD_ReturnType INT_ADC_deinit(void);
+STD_ReturnType INT_ADC_set_callback_routine(void (*callback) (uint16 * result));
+#endif
 
 #endif	/* MCAL_INTERNAL_INTERRUPT_H */
