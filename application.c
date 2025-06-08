3f694f2a4a90f6239cd14f820e2173a97adccaf4
Nourishere <nournawar5@gmail.com>
2025-06-08 13:59:45 +0300

    Updated application.c

--- application.c
+++ application.c
@@ -8,17 +8,24 @@
 #include "/home/nour/programs/microchip/xc8/v2.50/pic/include/builtins.h"   
 #include "MCAL_layer/ADC/hal_adc.h"
 #include "MCAL_layer/Interrupt/mcal_internal_interrupt.h"
-uint8 Iflag;
+#include "MCAL_layer/Interrupt/mcal_external_interrupt.h"
+volatile uint8 Iflag;
 /* We are operating in the HS configuration of the internal oscillator which has
   an clock speed of 8MHz/16MHz */
  //#define _XTAL_FREQ 8000000
 //This macro is used in the __delay_ms() function
 ADC_t ADC1 = {
 	__ADC,
-	INT_PLOW, 	
-	ADC_ch1,
+	//INT_PLOW, 	
+	ADC_ch7,
 	ADC_CCLOCK_FOSC_DIV_32,
 	1
+};
+INT_INTx_t INT_1 = {
+    __INT1,
+    {PORTB_I,PIN1,GPIO_IN,GPIO_LOW},
+    rising,
+   // INT_PLOW
 };
 
 STD_ReturnType ret;
@@ -27,9 +34,10 @@ int main(void){
 	if(E_NOT_OK == ret){
 		return -1; 
 	}
+        
     while(1){
-		LED_toggle(&LED_main);      
-		__delay_ms(1000);
+    	ADC_convert(&ADC1,ADC_default); 
+        __delay_us(10); /* Delay for acquisition */
 	}
     
 }
@@ -37,8 +45,8 @@ int main(void){
 STD_ReturnType application_initialize(){
     STD_ReturnType ret = E_OK;
     /* If you may initialize types that are locally defined */
-	ret = ecu_init() &&
-		  ADC_init(&ADC1);
+	ret = ecu_init();
+       ret = ret && ADC_init(&ADC1);
     return ret;
 }
 
@@ -51,7 +59,14 @@ void __INT2(void){
 void __ADC(uint16 * result){
 	/* do something when the ADC 
   	conversion is done */
-	uint16 lresult = *result; /* The result is stored */
-        
+	uint8 i = 0;	
+	uint16 lresult = *result; /* The result is stored here */
+
+	for(i=0;i<10;i++){
+		if(*result >> i & 1)
+			LED_on(&LED_arr[i]);
+          	else
+            		LED_off(&LED_arr[i]); 
+	}
 
 }
