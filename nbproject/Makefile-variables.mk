9ed5bbb77ccedca933491cb19aaca8d344dd7216
Nourishere <nournawar5@gmail.com>
2025-06-29 16:11:54 +0300

    Added build files

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=interfacing_main.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/interfacing_main.X.production.hex
