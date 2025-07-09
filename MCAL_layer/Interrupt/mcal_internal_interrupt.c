7c05be59f93eeae314f6b13fd426778a64b96807
Nourishere <nournawar5@gmail.com>
2025-07-09 18:56:23 +0300

    Added support for the Timer1 module.

--- MCAL_layer/Interrupt/mcal_internal_interrupt.c
+++ MCAL_layer/Interrupt/mcal_internal_interrupt.c
@@ -17,6 +17,17 @@ void ADC_ISR(void){
 		ADC_callback(&ADC_output);
 	}
 }
+extern uint16 preloaded_tmr1;
+void TMR0_ISR(void){
+	/* Flag is already clear */
+	/* Preload the registers */
+	TMR1H = (uint8)(preloaded >> 8);
+	TMR1L = (uint8)(preloaded & 0x00FF);
+	if(TMR1_callback){
+		TMR1_callback();
+	}
+	T0CONbits.TMR0ON=1; // Turn on the module
+}
 
 /* @brief: Initialize the ADC interrupt.
  * @param: A pointer to an ADC_t type.
@@ -58,7 +69,7 @@ STD_ReturnType INT_ADC_init(uint8 priority){
 
 /* @brief: De-initialize the ADC interrupt. 
  * @param: This function does not take any input.
- * @return: E_OK upon success and E_NOT_OK otherwise.
+ * @return: E_OK.
  */
 STD_ReturnType INT_ADC_deinit(void){
 	STD_ReturnType ret = E_OK;
@@ -69,11 +80,63 @@ STD_ReturnType INT_ADC_deinit(void){
 
 /* @brief: Set up the callback function for the ADC interrupt.
  * @param: A pointer to a ADC_t type.
- * @return: E_OK upon success and E_NOT_OK otherwise.
+ * @return: E_OK.
  */
 STD_ReturnType INT_ADC_set_callback_routine(void (*callback) (uint16 * result)){
 	STD_ReturnType ret = E_OK;
 		ADC_callback = callback;	
+	return ret;
+}
+#endif
+#if (INT_TMR1 == INT_EN)
+void (*TMR1_callback) (void) = NULL;
+/* @brief: Initialize the interrupt feature for the Timer1 module.
+ * @param: A uint8 specifying interrupt priority.
+ * @return: E_OK upon success and E_NOT_OK otherwise.
+ */
+STD_ReturnType INT_TMR1_init(uint8 priority){
+	STD_ReturnType ret = E_OK;
+	INT_GEN(); // Enable the interupt feature
+	/* Disable the interrupt */
+	INT_TMR1_DIS();
+	#if (INT_PR == INT_EN)
+	INT_PREN(); // Enable priority feature
+	if(priority == INT_PHIGH){
+		INT_GHPEN();
+		INT_TMR1_HP();
+	}
+	else if (priority == INT_PLOW){
+		INT_GLPEN();
+		INT_TMR1_LP();
+	}
+	#elif (INT_PR == INT_DIS)
+	INT_PRDIS();
+	INT_PEEN();  // Enable peripheral interupts
+	#else
+	ret = E_NOT_OK;
+	#endif
+	INT_TMR1_EN();
+	return ret;
+}
+
+/* @brief: Deinitialize the Timer1 module.
+ * @param: None.
+ * @return: E_OK.
+ */
+STD_ReturnType INT_TMR1_deinit(void){
+	STD_ReturnType ret = E_OK;
+	INT_TMR1_CLRF();
+	INT_TMR1_DIS();
+	return ret;
+}
+
+/* @brief: Set a callback routine for the Timer1 module.
+ * @param: A pointer to a function with void parameters and void return.
+ * @return: E_OK;
+ */
+STD_ReturnType INT_TMR1_set_callback_routine(void (*callback) (void)){
+	STD_ReturnType ret = E_OK;
+	TMR1_callback = callback;
 	return ret;
 }
 #endif
