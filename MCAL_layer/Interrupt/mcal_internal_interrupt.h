c459d232ae8193c3946c709f9f272a893fbe1267
Nourishere <nournawar5@gmail.com>
2025-07-22 21:30:29 +0300

    Added support for the Timer3 module.

--- MCAL_layer/Interrupt/mcal_internal_interrupt.h
+++ MCAL_layer/Interrupt/mcal_internal_interrupt.h
@@ -19,6 +19,10 @@
 #define INT_TMR1_STATUS (PIE1bits.TMR1IE)
 /* The value of the TMR1 flag */
 #define INT_TMR1_F (PIR1bits.TMR1IF)
+/* status of the TMR2 (EN/DIS) */
+#define INT_TMR2_STATUS (PIE1bits.TMR2IE)
+/* The value of the TMR2 flag */
+#define INT_TMR2_F (PIR1bits.TMR2IF)
 /****** function-like macros ******/
 #if (INT_ADC == INT_EN)
 /* Enable/Disable the ADC interrupt */
@@ -32,6 +36,7 @@
 #define INT_ADC_LP() (IPR1bits.ADIP=0)
 #endif
 #endif
+
 #if (INT_TMR1 == INT_EN)
 /* Enable/Disable the TMR1 interrupt */
 #define INT_TMR1_EN() (PIE1bits.TMR1IE=1)
@@ -44,6 +49,33 @@
 #define INT_TMR1_LP() (IPR1bits.TMR1IP=0)
 #endif
 #endif
+
+#if (INT_TMR2 == INT_EN)
+/* Enable/Disable the TMR2 interrupt */
+#define INT_TMR2_EN() (PIE1bits.TMR1IE=1)
+#define INT_TMR2_DIS() (PIE1bits.TMR1IE=0)
+/* Clear the TMR2 interrupt flag */
+#define INT_TMR2_CLRF() (PIR1bits.TMR2IF=0)
+/* Set the TMR2 priority */
+#if (INT_PR == INT_EN)
+#define INT_TMR2_HP() (IPR1bits.TMR1IP=1)
+#define INT_TMR2_LP() (IPR1bits.TMR1IP=0)
+#endif
+#endif
+
+#if (INT_TMR3 == INT_EN)
+/* Enable/Disable the TMR3 interrupt */
+#define INT_TMR3_EN() (PIE2bits.TMR3IE=1)
+#define INT_TMR3_DIS() (PIE2bits.TMR3IE=0)
+/* Clear the TMR3 interrupt flag */
+#define INT_TMR3_CLRF() (PIR2bits.TMR3IF=0)
+/* Set the TMR3 priority */
+#if (INT_PR == INT_EN)
+#define INT_TMR3_HP() (IPR2bits.TMR3IP=1)
+#define INT_TMR3_LP() (IPR2bits.TMR3IP=0)
+#endif
+#endif
+
 /***** Function prototypes *****/
 void ADC_ISR(void);
 #if (INT_ADC == INT_EN)
@@ -58,4 +90,17 @@ STD_ReturnType INT_TMR1_deinit(void);
 STD_ReturnType INT_TMR1_set_callback_routine(void (*callback) (void));
 #endif
 
+void TMR2_ISR(void);
+#if (INT_TMR2 == INT_EN)
+STD_ReturnType INT_TMR2_init(uint8 priority);
+STD_ReturnType INT_TMR2_deinit(void);
+STD_ReturnType INT_TMR2_set_callback_routine(void (*callback) (void));
+#endif
+
+void TMR3_ISR(void);
+#if (INT_TMR3 == INT_EN)
+STD_ReturnType INT_TMR3_init(uint8 priority);
+STD_ReturnType INT_TMR3_deinit(void);
+STD_ReturnType INT_TMR3_set_callback_routine(void (*callback) (void));
+#endif
 #endif	/* MCAL_INTERNAL_INTERRUPT_H */
