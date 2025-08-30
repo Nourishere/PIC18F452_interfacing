092de2dc709c4fc39305cdb6890a77f270a013e8
Nourishere <nournawar5@gmail.com>
2025-08-30 16:18:47 +0300

    Added _XTAL_FREQ macro and updated STD_ReturnType to include USART returns

--- MCAL_layer/mcal_std_types.h
+++ MCAL_layer/mcal_std_types.h
@@ -11,14 +11,19 @@
 #include "std_libs.h"
 #include "compiler.h"
 /*************** macros ********************/
+#define _XTAL_FREQ 8000000 /* The defined crystal clock frequency */
 #define STD_HIGH 0x01;
 #define STD_LOW  0x00;
 #define STD_ON  0x01;
 #define STD_OFF  0x00;
 #define STD_ACTIVE 0x01;
 #define STD_IDLE 0x00;
-#define E_OK (STD_ReturnType)(0x01)
-#define E_NOT_OK (STD_ReturnType)(0x00)
+#define E_OK (STD_ReturnType)(0x00)
+#define E_NOT_OK (STD_ReturnType)(0x01)
+#define E_OVR (STD_ReturnType)(0xFF) /* Overrun error (USART) */
+#define E_FERR (STD_ReturnType)(0xFE) /* Framming error (USART) */
+#define E_NOT_ADDRD (STD_ReturnType)(0xFD) /* USART not addressed */
+#define E_RX_PARITY_ERR (STD_ReturnType)(0xFC) /* Parity error at the receiver */
 /*************** function macros ***********/
 /*************** data types ****************/
 typedef unsigned char uint8;
