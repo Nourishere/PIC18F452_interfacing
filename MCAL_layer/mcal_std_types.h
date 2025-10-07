0f677f0036c0f6adb0a32263b638b57ddaad0777
Nourishere <nournawar5@gmail.com>
2025-10-07 22:48:13 +0300

    Added return types for MSSP I2C mode.

--- MCAL_layer/mcal_std_types.h
+++ MCAL_layer/mcal_std_types.h
@@ -12,18 +12,24 @@
 #include "compiler.h"
 /*************** macros ********************/
 #define _XTAL_FREQ 8000000 /* The defined crystal clock frequency */
-#define STD_HIGH 0x01;
-#define STD_LOW  0x00;
-#define STD_ON  0x01;
-#define STD_OFF  0x00;
-#define STD_ACTIVE 0x01;
-#define STD_IDLE 0x00;
-#define E_OK (STD_ReturnType)(0x00)
-#define E_NOT_OK (STD_ReturnType)(0x01)
+#define STD_HIGH 0x01
+#define STD_LOW  0x00
+#define STD_ON  0x01
+#define STD_OFF  0x00
+#define STD_ACTIVE 0x01
+#define STD_IDLE 0x00
+/* @note: The E_OK and E_NOT_OK are assigned as 1 and 0 so that
+ * 		  it is easier to AND results from routines with a 
+ * 		  STD_ReturnType type. (ret = ret && routine()). 
+ */
+#define E_OK (STD_ReturnType)(0x01)
+#define E_NOT_OK (STD_ReturnType)(0x00)
 #define E_OVR (STD_ReturnType)(0xFF) /* Overrun error (USART) */
 #define E_FERR (STD_ReturnType)(0xFE) /* Framming error (USART) */
 #define E_NOT_ADDRD (STD_ReturnType)(0xFD) /* USART not addressed */
 #define E_RX_PARITY_ERR (STD_ReturnType)(0xFC) /* Parity error at the receiver */
+#define E_BUS_COLL (STD_ReturnType)(0xFB) /* Bus collision (I2C) */
+#define E_WR_COLL (STD_ReturnType)(0xFA) /* Write collision (I2C) */
 /*************** function macros ***********/
 /*************** data types ****************/
 typedef unsigned char uint8;
