e4d8576adaaca17289ca7ce5a0400ac8fff47877
Nourishere <nournawar5@gmail.com>
2025-10-09 16:49:58 +0300

    Use mcal_std_types.h macros

--- MCAL_layer/GPIO/hal_gpio_cfg.h
+++ MCAL_layer/GPIO/hal_gpio_cfg.h
@@ -3,17 +3,16 @@
  * Author: nour
  *
  * Created on November 1, 2024, 6:56 AM
+ * @brief: This file configures the function of the GPIO module.
  */
 
 #ifndef HAL_GPIO_CFG_H
 #define	HAL_GPIO_CFG_H
-
+/********* includes *********/
+#include "../mcal_std_types.h"
 /********* macros ***********/
-#define ENABLED 1
-#define DISABLED 0
-#define PIN_CONFIGURATION ENABLED
-#define PORT_CONFIGURATION ENABLED
-
+#define PIN_CONFIGURATION STD_ON 
+#define PORT_CONFIGURATION STD_ON 
 
 #endif	/* HAL_GPIO_CFG_H */
 
